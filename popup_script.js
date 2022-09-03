0c941343332a083a28c7a154ff5cd4b1387d368b
Lance1738 <71152123+Lance1738@users.noreply.github.com>
2022-09-03 17:33:41 -0400

    changed ui, updated logic for popup/main script

--- popup_script.js
+++ popup_script.js
@@ -3,57 +3,91 @@ window.onload = function () {
   document.getElementById("clearButton").onclick = clear;
 };
 
-var placeholder_link = "www.youtube.com";
+setInterval(function () {
+  chrome.storage.local.get(["current_website"], function (items) {
+    //console.log(items.current_website);
+
+    var current = items.current_website;
+
+    console.log(current);
+
+    if (document.getElementById("currentLink").innerHTML !== current) {
+      document.getElementById("currentLink").innerHTML = current;
+    }
+  });
+}, 1000);
+
+/*
+
+setInterval(function() {
+
+
+  chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+  
+  
+      var cur_link = request.greeting;
+      console.log(request.greeting);
+  
+      document.getElementById("currentLink").innerHTML = cur_link;
+  
+    
+      console.log(sender.tab ?
+                  "from a content script:" + sender.tab.url :
+                  "from the extension");
+  
+                  
+      if (request.greeting == "hello")
+        sendResponse({farewell: "goodbye"});
+    }
+  );
+  
+
+}, 1000)
+*/
+
+const placeholder_link = "www.youtube.com";
 
 // Checks if key exists
 
 if (localStorage.getItem("website_names") !== null) {
-
   var x = localStorage.getItem("website_names");
-  
+
   var formatted = x.toString().slice(1, -1).replace(/['"]+/g, "");
 
   // splitted = local storage array
   var splitted = formatted.toString().split(",");
 
-
   if (splitted[0] === "") {
     document.getElementById("textbox").placeholder = placeholder_link;
-  }
-  else {
+  } else {
     document.getElementById("textbox").value = splitted[0];
   }
 
   if (splitted[1] === "") {
     document.getElementById("textbox2").placeholder = placeholder_link;
-  }
-  else {
+  } else {
     document.getElementById("textbox2").value = splitted[1];
   }
 
   if (splitted[2] === "") {
     document.getElementById("textbox3").placeholder = placeholder_link;
-  }
-  else {
+  } else {
     document.getElementById("textbox3").value = splitted[2];
   }
 
   if (splitted[3] === "") {
     document.getElementById("textbox4").placeholder = placeholder_link;
-  }
-  else {
+  } else {
     document.getElementById("textbox4").value = splitted[3];
   }
-  
+
   if (splitted[4] === "") {
     document.getElementById("textbox5").placeholder = placeholder_link;
-  }
-  else {
+  } else {
     document.getElementById("textbox5").value = splitted[4];
   }
-
-}
-else {
+} else {
   document.getElementById("textbox").placeholder = placeholder_link;
   document.getElementById("textbox2").placeholder = placeholder_link;
   document.getElementById("textbox3").placeholder = placeholder_link;
@@ -61,24 +95,83 @@ else {
   document.getElementById("textbox5").placeholder = placeholder_link;
 }
 
-
+// !parseLink(name)
 function save() {
   var name = document.getElementById("textbox").value.toString().toLowerCase();
-  var name2 = document.getElementById("textbox2").value.toString().toLowerCase();
-  var name3 = document.getElementById("textbox3").value.toString().toLowerCase();
-  var name4 = document.getElementById("textbox4").value.toString().toLowerCase();
-  var name5 = document.getElementById("textbox5").value.toString().toLowerCase();
+
+  if (name.toString().length !== 0) {
+    if (!parseLink(name)) {
+      document.getElementById("errorMessage").innerHTML = "Invalid URL";
+    } else {
+      console.log("in here");
+      document.getElementById("errorMessage").innerHTML = "";
+    }
+  }
+
+  var name2 = document
+    .getElementById("textbox2")
+    .value.toString()
+    .toLowerCase();
+
+  if (name2.toString().length !== 0) {
+    console.log(name2.toString().length);
+    if (!parseLink(name2)) {
+      document.getElementById("errorMessage").innerHTML = "Invalid URL";
+    } else {
+      console.log("in here");
+      document.getElementById("errorMessage").innerHTML = "";
+    }
+  }
+  var name3 = document
+    .getElementById("textbox3")
+    .value.toString()
+    .toLowerCase();
+
+  if (name3.toString().length !== 0) {
+    if (!parseLink(name3)) {
+      document.getElementById("errorMessage").innerHTML = "Invalid URL";
+    } else {
+      console.log("in here");
+      document.getElementById("errorMessage").innerHTML = "";
+    }
+  }
+
+  var name4 = document
+    .getElementById("textbox4")
+    .value.toString()
+    .toLowerCase();
+
+  if (name4.toString().length !== 0) {
+    if (!parseLink(name4)) {
+      document.getElementById("errorMessage").innerHTML = "Invalid URL";
+    } else {
+      console.log("in here");
+      document.getElementById("errorMessage").innerHTML = "";
+    }
+  }
+
+  var name5 = document
+    .getElementById("textbox5")
+    .value.toString()
+    .toLowerCase();
+
+  if (name5.toString().length !== 0) {
+    if (!parseLink(name5)) {
+      document.getElementById("errorMessage").innerHTML = "Invalid URL";
+    } else {
+      console.log("in here");
+      document.getElementById("errorMessage").innerHTML = "";
+    }
+  }
 
   var names_formatted =
     '["' + name + "," + name2 + "," + name3 + "," + name4 + "," + name5 + '"]';
-
 
   document.getElementById("textbox").value = name;
 
   if (localStorage.getItem("website_names") == null) {
     localStorage.setItem("website_names", "[]");
   }
-
   localStorage.setItem("website_names", names_formatted);
 }
 
@@ -94,4 +187,15 @@ function clear() {
   document.getElementById("textbox3").placeholder = placeholder_link;
   document.getElementById("textbox4").placeholder = placeholder_link;
   document.getElementById("textbox5").placeholder = placeholder_link;
+}
+
+function parseLink(url) {
+  var isValid = -1;
+
+  const url_pattern1 = new RegExp("^[www]+[.]");
+
+  isValid = url_pattern1.test(url);
+  console.log(isValid);
+
+  return isValid;
 }
--- script.js
+++ script.js
@@ -1,93 +1,138 @@
-const generateHTML0 = (websiteName) => {
-  console.log("Inside func call");
-
-  return ` 
-      
-    <style>
-  
-    html, body {
-      background-color: #191B1C !important;
-    }
-    
-    </style>
-  
-      <body>
-  
-      <div style = "display: flex; align-items:center; justify-content:center;" class = "title">
-  
-      <h1  id = "head" style = "color:red; font-size:64px;"> GET BACK TO STUDYING! NO SOCIAL MEDIA! </h1>
-  
-     </div>
-  
-  
-     <div style = "display: flex;  align-items:center; justify-content:center" class = "vid">
-     
-      <iframe style = "pointer-events: none;" width="560" height="315" src="https://www.youtube.com/embed/cnrxJtJcSew?start=91&autoplay=1&showinfo=0&controls=0&loop=1"   
-      allow="autoplay;" frameborder="0" allowfullscreen></iframe>
-  
-    </div>
-  
-      </body>
-  
-      `;
-};
-
-function getRandomNumber(max) {
-  return Math.floor(Math.random() * max);
-}
-
-// recieve
-
-// Read it using the storage API
-
-console.log(window.location.hostname);
-
+/*
 setInterval(function() {
 
-chrome.storage.local.get(["websites"], function (items) {
+  chrome.runtime.sendMessage({greeting: cur}, function(response) {
+    console.log(response.farewell);
+  });
+
+}, 1000)
+*/
+
+setInterval(function () {
+  var cur = window.location.hostname;
+
+  console.log(cur);
+
+  cur = parseCurrent(cur);
+
+  console.log(cur);
 
 
-  // gets rid of parenthesis/quoutes
-  var formatted = items.websites.toString().slice(1, -1).replace(/['"]+/g, "");
+  chrome.storage.local.set({["current_website"]: cur }, function () {});
 
-  // splitted is an array
-  var splitted = formatted.toString().split(",");
+  chrome.storage.local.get(["websites"], function (items) {
 
-  var website1 = splitted[0];
-  var website2 = splitted[1];
-  var website3 = splitted[2];
-  var website4 = splitted[3];
-  var website5 = splitted[4];
+    // gets rid of parenthesis/quoutes
+    var formatted = items.websites
+      .toString()
+      .slice(1, -1)
+      .replace(/['"]+/g, "");
 
-  //console.log(items.websites.toString().slice(1, -1).replace(/['"]+/g, ""));
-  //console.log(splitted);
+      console.log(formatted);
 
-  switch (window.location.hostname) {
-    case website1:
+    // splits each of the 5 urls into it's own array index
+    var splitted = formatted.toString().split(",");
+
+    var website1 = splitted[0];
+    var website2 = splitted[1];
+    var website3 = splitted[2];
+    var website4 = splitted[3];
+    var website5 = splitted[4];
+
+
+
+
+
+    if (website1 == cur) {
       console.log("tyest");
-      window.location.replace("https://lancema03.github.io/Redirect-Website/");
-      break;
-    case website2:
+        window.location.replace(
+          "https://lancema03.github.io/Redirect-Website/"
+        );
+        
+    }
+    else if (website2 == cur) {
       console.log("tyest");
-      window.location.replace("https://lancema03.github.io/Redirect-Website/");
-      break;
+        window.location.replace(
+          "https://lancema03.github.io/Redirect-Website/"
+        );
+        
+    }
+    else if (website3 == cur) {
+      console.log("tyest");
+        window.location.replace(
+          "https://lancema03.github.io/Redirect-Website/"
+        );
+        
+    }
+    else if (website4 == cur) {
+      console.log("tyest");
+        window.location.replace(
+          "https://lancema03.github.io/Redirect-Website/"
+        );
+        
+    }
+    else if (website5 == cur) {
+      console.log("tyest");
+        window.location.replace(
+          "https://lancema03.github.io/Redirect-Website/"
+        );
+    }
 
-    case website3:
-      console.log("tyest");
-      window.location.replace("https://lancema03.github.io/Redirect-Website/");
-      break;
 
-    case website4:
-      console.log("tyest");
-      window.location.replace("https://lancema03.github.io/Redirect-Website/");
-      break;
+    /*
+    switch (window.location.hostname) {
+      case website1:
+        console.log("tyest");
+        window.location.replace(
+          "https://lancema03.github.io/Redirect-Website/"
+        );
+        break;
+      case website2:
+        console.log("tyest");
+        window.location.replace(
+          "https://lancema03.github.io/Redirect-Website/"
+        );
+        break;
 
-    case website5:
-      console.log("tyest");
-      window.location.replace("https://lancema03.github.io/Redirect-Website/");
-      break;
-  }
-});
+      case website3:
+        console.log("tyest");
+        window.location.replace(
+          "https://lancema03.github.io/Redirect-Website/"
+        );
+        break;
+
+      case website4:
+        console.log("tyest");
+        window.location.replace(
+          "https://lancema03.github.io/Redirect-Website/"
+        );
+        break;
+
+      case website5:
+        console.log("tyest");
+        window.location.replace(
+          "https://lancema03.github.io/Redirect-Website/"
+        );
+        break;
+        
+    }
+    */
+  });
 }, 1000);
+
+
+
+function parseCurrent (currentURL) {
+
+  const pre = "www.";
+
+  if (!currentURL.toString().startsWith("www.")) {
+    var newLink = pre + currentURL;
+    console.log(newLink);
+    return newLink;
+  }
+  return currentURL; 
+}
+
 
 console.log("This is from main script");
